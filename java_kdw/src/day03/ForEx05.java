e6b62119fb7f0196d8181682617761c690d80a19
kdw6052 <kdw6052@naver.com>
2022-10-26 21:51:08 +0900

    for

--- java_kdw/src/day03/ForEx05.java
+++ java_kdw/src/day03/ForEx05.java
@@ -13,7 +13,7 @@ public class ForEx05 {
 		for(i=1;i<=10;i++) {
 			sum=sum +i;//sum+=i;
 		}
-		System.out.println("1부터 10까지의 합은 "+sum+ " 입니다.");
+		System.out.println("1부터 10까지의 합은 "+sum+ "입니다.");
 	}
 
 }
