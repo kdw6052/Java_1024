2ed35ace072d9f129526e2d7c705699a99811af1
kdw6052 <kdw6052@naver.com>
2022-11-01 21:37:50 +0900

    자바 07일차:  method

--- java_kdw/src/day07/MethodArrayEx03.java
+++ java_kdw/src/day07/MethodArrayEx03.java
@@ -4,21 +4,12 @@ public class MethodArrayEx03 {
 
 	public static void main(String[] args) {
 		// 배열에 정수 num가 있는지 없는지 확인하는 코드를 작성하세요. 메소드 이용
-		int min =1,max=9;
-		int arr1[] =new int[3];
-		createRandomArray(arr1, min, max);
-
-	}
-	public static void printArray(int arr[]) {
-		for(int i =0;i<arr.length;i++) {
-			System.out.print(arr[i]+" ");
-		}
-		System.out.println();
-	}
-	public static void createRandomArray(int arr[], int min, int max) {
-		
-		for(int i=0;i<arr.length;i++) {
-			arr[i]=random(min,max);
+		int arr[] = {1,3,5,7,9};
+		int num = 2;
+		if(check(arr, num)) {
+			System.out.println(num+"는 배열에 있습니다.");
+		}else {
+			System.out.println(num+"는 배열에 없습니다.");
 		}
 	}
 	/* 기능 : 배열에 정수 num가 있는지 없는지 알려주는 메소드
@@ -26,10 +17,12 @@ public class MethodArrayEx03 {
 	 * 리턴타입 : boolean
 	 * 메소드명 :	check
 	 * */
-	/*public static boolean check(int arr[], int num){
-		for
-	}*/
-	public static int random(int min,int max) {
-		return (int)(Math.random()* (max - min + 1)+min);
+	public static boolean check(int arr[], int num){
+		for(int tmp:arr) {
+			if(num==tmp) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
