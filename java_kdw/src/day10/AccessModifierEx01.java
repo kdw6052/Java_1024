e09121b1b3922457e27ea4eeb637a51b13e0160d
kdw6052 <kdw6052@naver.com>
2022-11-04 21:19:08 +0900

    자바10일차:접근제한자 설명

---
+++ java_kdw/src/day10/AccessModifierEx01.java
@@ -0,0 +1,10 @@
+package day10;
+
+public class AccessModifierEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java_kdw/src/day10_1/TestB.java
@@ -0,0 +1,5 @@
+package day10_1;
+
+public class TestB {
+
+}
