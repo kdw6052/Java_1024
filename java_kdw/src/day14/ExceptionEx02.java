2b66fac6401ad4fcb1187d5ae0aae8262b864693
kdw6052 <kdw6052@naver.com>
2022-11-10 21:33:11 +0900

    자바 14일차 : 예외처리,리스트

---
+++ java_kdw/src/day14/ExceptionEx02.java
@@ -0,0 +1,25 @@
+package day14;
+
+import java.util.Scanner;
+
+public class ExceptionEx02 {
+
+	public static void main(String[] args) {
+		/* Scanner를 통해 정수를 입력 받아 입력받은 정수를 출력하는 코드를 작성하세요.
+		 * 정수 대신 문자열을 입력하면 예외가 발생하는데, 이 예외를 처리하는 코드를 작성*/
+		Scanner sc = new Scanner(System.in);
+		System.out.print("정수 입력 : ");
+		int num1=0;
+		try {
+			num1= sc.nextInt();
+			System.out.println("입력된 정수 : "+num1);
+		}catch(Exception e) {
+			System.out.println("정수를 입력하세요");
+		}
+		sc.close();
+	}
+	
+	
+}
+
+	
