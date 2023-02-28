7950472aa1d5542e729157345844e7b875c30bd4
kdw6052 <kdw6052@naver.com>
2023-02-28 21:25:46 +0900

    스프링 15일차 : 게시글 수정(연결까지 구현)

--- spring project/spring/src/main/java/kr/kh/spring/service/BoardService.java
+++ spring project/spring/src/main/java/kr/kh/spring/service/BoardService.java
@@ -29,4 +29,6 @@ public interface BoardService {
 	LikesVO getLikes(int bo_num, MemberVO user);
 
 	boolean deleteBoard(int bo_num, MemberVO user);
+
+	BoardVO getBoardByWriteAuthority(int bo_num, MemberVO user);
 }
